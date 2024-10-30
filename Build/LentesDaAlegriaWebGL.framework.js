97591fa316c6391cf7d6f589ce577c94a48a4a66
brwolfless <renangusman@hotmail.com>
2024-10-30 16:11:24 -0300

    build

--- Build/LentesDaAlegriaWebGL.framework.js
+++ Build/LentesDaAlegriaWebGL.framework.js
@@ -1993,13 +1993,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4377632: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4377693: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4377757: function() {return Module.webglContextAttributes.powerPreference;},  
- 4377815: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4377870: function($0) {performance.now = function() { return $0; };},  
- 4377918: function($0) {performance.now = function() { return $0; };},  
- 4377966: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4378784: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4378845: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4378909: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4378967: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4379022: function($0) {performance.now = function() { return $0; };},  
+ 4379070: function($0) {performance.now = function() { return $0; };},  
+ 4379118: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
